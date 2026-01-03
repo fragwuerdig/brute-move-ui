3b57d63118d25342398b29263c051f207ee8d454
Till Ziegler <dev@luncgoblins.com>
2026-01-03 15:52:51 +0100

    feat: telegram notification bell

--- src/Head.tsx
+++ src/Head.tsx
@@ -1,6 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import { useWallet } from "./WalletProvider";
 import { addressEllipsis } from "./Common";
+import { config } from "./config";
 import HeaderMenu from "./HeaderMenu";
 import Footer from "./Footer";
 import pawnLogo from "./assets/pawn.png";
@@ -11,6 +12,13 @@ const DisconnectIcon = () => (
         <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4" />
         <polyline points="16 17 21 12 16 7" />
         <line x1="21" y1="12" x2="9" y2="12" />
+    </svg>
+);
+
+const BellIcon = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+        <path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9" />
+        <path d="M13.73 21a2 2 0 0 1-3.46 0" />
     </svg>
 );
 
@@ -31,10 +39,21 @@ function Head({ children }: { children?: React.ReactNode }) {
                     <div className="header-nav">
                         <HeaderMenu />
                         {connected && connectedAddr && (
-                            <button className="header-wallet" onClick={disconnect}>
-                                <span className="header-wallet__address">{addressEllipsis(connectedAddr)}</span>
-                                <DisconnectIcon />
-                            </button>
+                            <>
+                                <a
+                                    href={`https://t.me/${config.telegramBotUsername}?start=${connectedAddr}`}
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    className="header-notifications"
+                                    title="Enable notifications"
+                                >
+                                    <BellIcon />
+                                </a>
+                                <button className="header-wallet" onClick={disconnect}>
+                                    <span className="header-wallet__address">{addressEllipsis(connectedAddr)}</span>
+                                    <DisconnectIcon />
+                                </button>
+                            </>
                         )}
                     </div>
                 </div>
